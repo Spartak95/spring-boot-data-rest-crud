2379935ed963ac31fa310907db99550b26a7df0a
Spartak95 <gareginyans@mail.ru>
2021-03-01 12:16:20 +0400

    Initial commit

---
+++ src/main/java/com/spring/exception_handling/ProductIncorrectData.java
@@ -0,0 +1,16 @@
+package com.spring.exception_handling;
+
+public class ProductIncorrectData {
+    private String info;
+
+    public ProductIncorrectData() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
